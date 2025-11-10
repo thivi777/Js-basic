96b8bf86847c8de92ebe6ae208df292b3c87c329
“thivi777” <angelthivi9@gmail.com>
2025-11-10 11:45:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+//  hosting environment:
+ console.log(name);
+var name = "Alice";
+   
+console.log(age);
+let age = 30;
+
+console.log(city);
+const city = "New York";
+
+// scope example:
+
+{ var x = 25;
+    console.log(x);
+}
+console.log( x);
+
+{ const x = 25;
+    console.log(x);
+}
+console.log( x);
+
+
+
+{ let x = 25;
+    console.log(x);
+}
+console.log( x);
